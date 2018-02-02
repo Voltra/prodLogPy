49adfd98de94069045d084fd76b7e4cab5637748
Ptipiak <rahael_b-p@hotmail.fr>
2018-02-02 16:16:01 +0100

    adding main.py

--- exos/RBP/main.py
+++ exos/RBP/main.py
@@ -1,3 +1,35 @@
-from calculus import *
+def toArray(filePath, symbol=""):
+    array = []
+    f = open(filePath, "r")
+    if symbol=="":
+        while f.readable():
+            line = f.readline()
+            for obj in line:
+                array.append(obj)
+    else:
+        while f.readable():
+            line = f.readline()
+            tmp = line.split(symbol)
+            array.extend(tmp)
+    return array
 
-calculus()
+def formatInt(array):
+    for i in range(0,len(array)):
+        array[i] = int(array[i])
+    return array
+
+def toString(array,symbol=""):
+    tmp = ""
+    if symbol =="":
+        for obj in array:
+            tmp += str(obj)
+    else:
+        for obj in array:
+            tmp += str(obj)
+            tmp += " "
+    print(tmp)
+
+tab = toArray("../rsc/test_val"," ")
+tab = formatInt(tab)
+toString(tab," ")
+print(sum(tab))
