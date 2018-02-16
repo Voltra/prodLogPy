eabafd72a5330f7ad01b2ab753a30fa1b6ae87c6
Voltra <ludwig.guerin.98@gmx.fr>
2018-02-16 14:54:47 +0100

    Started working on the website back-end, added sql queries folder, etc...

--- app/models/Model.py
+++ app/models/Model.py
@@ -5,7 +5,8 @@ class Model:
     """
     @:param connection being a sqlite3 connection
     """
-    def __init__(self, connection):
+    def __init__(self, tableName, connection):
+        self.table = tableName
         self.co = connection
         self.cursor = self.co.cursor()
     #
