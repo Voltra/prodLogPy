ac2b45fb2029c01492620e36d5adbc8f13eaa420
Voltra <ludwig.guerin.98@gmx.fr>
2018-03-16 14:33:29 +0100

    Documented a bit of code

--- app/apimodels/ApiModel.py
+++ app/apimodels/ApiModel.py
@@ -10,11 +10,22 @@ A class that factorizes the behavior of models used for the API
 class ApiModel(Model):
     LIMIT = 20
 
+    """
+    Construct an ApiModel from meta data of a DB table
+    @:param idCol being the column that serves as an ID
+    @:param tableName being the name of the table bound to this ApiModel
+    @:param connection being an SQLite3 DB connection
+    """
     def __init__(self, idCol, tableName, connection):
         super().__init__(tableName, connection)
         self.id = idCol
     #
 
+    """
+    Determines whether or not there's an item that has the given id in th DB tabe
+    @:param _id being the ID to test
+    @:returns A response tuple (Boolean, Status), True if it exists, False otherwise ; a status 200 if everything went well, 400 if there were an error
+    """
     def exists(self, _id):
         query = "SELECT * FROM `%s` WHERE `%s` like ? LIMIT 1" % (self.table, self.id)
 
@@ -29,6 +40,11 @@ class ApiModel(Model):
         return length != 0, 200
     #
 
+    """
+    Retrieve the data associated to the given ID in the bound table
+    @:param _id being the ID of the tuple to retrieve
+    @:returns a response tuple (Json, Status), the data associated to the tuple ; 200 if there were no error, 400 if there were any
+    """
     def get(self, _id):
         if not self.exists(_id):
             abort(404, message="No resource matching the given id")
@@ -44,6 +60,10 @@ class ApiModel(Model):
         return self.cursor.fetchall(), 200
     #
 
+    """
+    Retrieve "all" the data from the bound table (limited by a limit amount)
+    @:returns a response tuple (Json, Status), the data associated to the tuple ; 200 if there were no error, 400 if there were any
+    """
     def getAll(self):
         query = "SELECT * FROM %s LIMIT %d" % (self.table, ApiModel.LIMIT)
 
--- app/apimodels/Equipement.py
+++ app/apimodels/Equipement.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 from app.apimodels.ApiModel import ApiModel
 
+"""
+The ApiModel for the 'equipements' table, using the 'EquipementId' column as the ID column
+"""
 class Equipement(ApiModel):
+    """
+    Instantiate a Equipement object
+    @:param connection being a SQLite3 DB connection
+    """
     def __init__(self, connection):
         super().__init__(idCol="EquipementId", tableName="equipements", connection=connection)
     #
