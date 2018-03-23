37106569ca3d71ca35e00967975fbf51a93c4bd3
Ptipiak <rahael_b-p@hotmail.fr>
2018-03-23 14:25:15 +0100

    from javascript to Jquery

--- app/js/index.js
+++ app/js/index.js
@@ -3,27 +3,28 @@ import {$json} from "@voltra/json"
 
 (_ => {
 	console.log("ready");
-	const form = document.querySelector("#searchForm");
-	form.addEventListener("submit", e => {
+	const $form = $("#searchForm");
+	$form.on("submit", e => {
 		e.preventDefault();
 		const url = "https://reqres.in/api/users";
 
-		const input = form.querySelector("input[name='query']");
-		const errorField = document.querySelector("#errorField");
+		const $input = $form.children("input[name='query']");
+		const $errorField = $("#errorField");
 
-		if (input.value === "") {
+		if ($input.value === "") {
 			console.log("error");
-			errorField.classList.add("error");
-			errorField.append("PINGAS");
+			$errorField.addClass("" +
+				"error");
+			$errorField.append("PINGAS");
 			return false
 		}
 
 		$json.post(url, {
 			email: "toto@titi.tata",
-			password: input.value
+			password: $input.value
 		}).then(jsonData => {
 			console.log(jsonData);
-			form.closest("div").append(jsonData);
+			$form.closest("div").append(jsonData);
 		}).catch(console.error);
 		return false;
 	});
